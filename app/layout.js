1209e35b71e8e34c6ed8cd8c521969012d4ed4d0
Subham Tibra <shubh.tibra@gmail.com>
2024-10-01 13:05:51 +0530

    remove dependency on experimental-nextjs-app-support
    dummy commit
    
    debug log
    
    fix api issue
    
    add loading icon
    
    Update README.md
    
    Update README.md

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,33 @@
 import Link from "next/link";
-import { ApolloWrapper } from "./apolloWrapper";
 import styles from "./styles/layout.module.css";
+import { ApolloWrapper } from "./utils/apolloWrapper";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={styles.body}>
-        <div className={styles.layout}>
-          <aside className={styles.sidebar}>
-            <nav>
-              <ul>
-                <li>
-                  <Link href="/books" className={styles.navLink}>
-                    Books
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/authors" className={styles.navLink}>
-                    Authors
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </aside>
-          <ApolloWrapper>
+        <ApolloWrapper>
+          <div className={styles.layout}>
+            <aside className={styles.sidebar}>
+              <nav>
+                <ul>
+                  <li>
+                    <Link href="/books" className={styles.navLink}>
+                      Books
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/authors" className={styles.navLink}>
+                      Authors
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </aside>
+
             <main className={styles.mainContent}>{children}</main>
-          </ApolloWrapper>
-        </div>
+          </div>
+        </ApolloWrapper>
       </body>
     </html>
   );
